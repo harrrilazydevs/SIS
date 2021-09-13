68e330a2cef5c9d1da745a0a80427961143fef6b
kdlanguido <kdlanguido@gmail.com>
2021-09-14 00:42:19 +0800

    push

--- api/dummy-test.php
+++ api/dummy-test.php
@@ -7,5 +7,5 @@ header('Content-Type: application/json');
 
 
 // echo json_encode(['status'=>200,'feedback'=>'Success!']);
-// echo json_encode(['status'=>403,'feedback'=>'FAil!']);
-echo json_encode(['status'=>500,'feedback'=>'Please contact admin']);
+echo json_encode(['status'=>403,'feedback'=>'FAil!']);
+// echo json_encode(['status'=>500,'feedback'=>'Please contact admin']);
